87122442485d1deb53ad2aeff2393a27462a8bbe
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-11-16 19:13:12 +0530

    Fix bugs

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,13 +10,16 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: "https://tejas-instagram.vercel.app",
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://tejas-instagram.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use("/v1", masterRouter);
 
