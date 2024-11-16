91024d3a9951d6b891b8d4c1d4eaafb3f4db3f80
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-11-16 19:21:41 +0530

    Fix deployment bugs(2)

--- server/src/routes/masterRouter.ts
+++ server/src/routes/masterRouter.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import postRouter from "./postRouter";
 import authRouter from "./authRouter";
-import commentRouter from "./commentRouter";
 import userRouter from "./userRouter";
 import authVerify from "../middlewares/authVerify";
 import likeRouter from "./likeRouter";
@@ -10,7 +9,6 @@ const masterRouter = express.Router();
 
 masterRouter.use("/auth", authRouter);
 masterRouter.use("/posts", authVerify, postRouter);
-masterRouter.use("/comments", authVerify, commentRouter);
 masterRouter.use("/users", authVerify, userRouter);
 masterRouter.use("/like", authVerify, likeRouter);
 
